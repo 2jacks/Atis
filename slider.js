b6444e8210c17099bc925d04b880da9af800721d
2jacks <anton.nozdrin.12@mail.ru>
2020-03-28 15:31:53 +0500

    Another Finall

---
+++ slider.js
@@ -0,0 +1,13 @@
+let slider = $("#reviewsSlider");
+
+slider.slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  arrows: false,
+  adaptiveHeight: true,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
